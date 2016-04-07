58a30d5c977af6a8613deb0dfe0b1ffdcea78b0d
Anthony Francis <anthony@campusboard.io>
2016-04-07 13:54:39 +0100

    add investor boolean to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406082613) do
+ActiveRecord::Schema.define(version: 20160407124553) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160406082613) do
     t.string   "whatido"
     t.string   "achievements"
     t.string   "lookingfor"
+    t.boolean  "investor"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
