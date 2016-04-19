10c28416b9b34df4897917b3dcd4c4667ab6f769
Anthony Francis <anthony@campusboard.io>
2016-04-19 19:49:34 +0100

    scope added

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,4 +1,7 @@
 class Form < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :syndicate
+
+	scope :confirm_backer, -> {where(confirm_backer: true).sum(:investment_pledge)}
+
 end
--- app/models/syndicate.rb
+++ app/models/syndicate.rb
@@ -1,4 +1,12 @@
 class Syndicate < ActiveRecord::Base
 	belongs_to :user
 	has_many :forms, dependent: :destroy
+
+	scope :confirm_backer, -> {where(confirm_backer: true).sum(:investment_pledge)}
+
+	
+	def total_confirm_pledges
+    	self.form.sum(:investment_pledge)
+  	end
+
 end
