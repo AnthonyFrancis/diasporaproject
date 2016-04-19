9ce91dda3ecc140a078182ad6aa54e728113d3b6
Anthony Francis <anthony@campusboard.io>
2016-04-19 22:22:45 +0100

    test

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,6 +2,14 @@ class Form < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :syndicate
 
-	scope :confirm_backer, -> {where(confirm_backer: true).sum(:investment_pledge)}
+	scope :confirm_backer, -> {where(confirm_backer: true)}
 
+	def pledge
+		pledge = self.investment_pledge.all
+		pledge.map 
+	end
+
+	def test_now
+		where('confirm_backer = ?', true ).sum(:investment_pledge)
+	end
 end
--- app/models/syndicate.rb
+++ app/models/syndicate.rb
@@ -2,4 +2,15 @@ class Syndicate < ActiveRecord::Base
 	belongs_to :user
 	has_many :forms, dependent: :destroy
 
+
+	def list
+	array = Array.new
+    forms = self.forms
+	    forms.all.each do |investment|
+	      array << investment.investment_pledge
+	    end
+	    array.to_s.gsub('"', '')
+	    total_sales = array.map {|s| s['sale_price']}.reduce(0, :+)
+  	end
+
 end
