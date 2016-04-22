c3bff940e55040fb45915df88c5b4cdc626dadf8
Anthony Francis <anthony@campusboard.io>
2016-04-22 14:46:50 +0100

    made boolean default false

--- db/migrate/20160414094805_add_confirmbacker_to_forms.rb
+++ db/migrate/20160414094805_add_confirmbacker_to_forms.rb
@@ -1,5 +1,5 @@
 class AddConfirmbackerToForms < ActiveRecord::Migration
   def change
-    add_column :forms, :confirm_backer, :boolean, default: 0
+    add_column :forms, :confirm_backer, :boolean, default: false
   end
 end
