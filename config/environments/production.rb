c225922cd0a2d3c7c5245ac473e75c6f618520a7
Anthony Francis <tonytrack@gmail.com>
2019-11-16 01:06:35 +0000

    changed home page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,13 @@ Rails.application.configure do
 
   # mailer
   config.action_mailer.default_url_options = { host: Rails.application.credentials.production[:host] }
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
