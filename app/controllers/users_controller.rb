442bf2a06fe27fc03242b638f92aa67ac820200b
Anthony Francis <tonytrack@gmail.com>
2019-12-02 12:55:45 +0000

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ end
 
 def index
 	@users = User.all
-	@profiles = Profile.all
+	@profiles = Profile.order("created_at DESC").paginate(:page => params[:page], :per_page => 1)
 	@combo =  @profiles + @users
 end
 
