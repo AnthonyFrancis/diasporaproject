4c2056e11b52802cf5ce8db8c1f7bd377234e786
Anthony Francis <tonytrack@gmail.com>
2019-12-02 14:03:48 +0000

    add promo footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
   	@syndicates = Syndicate.all
   	@users = User.all
-    @profiles = Profile.order("created_at DESC").paginate(:page => params[:page], :per_page => 1)
+    @profiles = Profile.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   end
 
   def about
